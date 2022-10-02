d0b972cffa494633782684418d2dbe5603d3a58e
Gentrit Uka <gentrit-uka@live.com>
2022-10-02 03:13:29 +0200

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css'
 export default function App() {
   // include all the characters and numbers in the password
 
+  const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [passwordLength, setPasswordLength] = useState(10)
 
@@ -14,6 +15,20 @@ export default function App() {
       password += characters.charAt(Math.floor(Math.random() * characters.length))
     }
     setPassword(password)
+  }
+
+  function generateEmail(){
+    // email should be in this format - email@email.com
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    let email = ''
+    for (let i = 0; i < 10; i++) {
+      email += characters.charAt(Math.floor(Math.random() * characters.length))
+    }
+    email += '@'
+    let domain = ['gmail.com', 'yahoo.com', 'hotmail.com', 'outlook.com']
+    let randomDomain = domain[Math.floor(Math.random() * domain.length)]
+    email += randomDomain
+    setEmail(email)
   }
 
   function handlePasswordLength(value: number) {
@@ -28,11 +43,18 @@ export default function App() {
         <button onClick={() => {
           generatePassword()
         }}>Generate Password</button>
-        </div>
+      </div>
       <div className="length">
         <label htmlFor="length">Password Length</label>
         <input type="number" value={passwordLength} onChange={(e) => handlePasswordLength(e.target.value)} />
       </div>
+      <div className="email">
+        <h2>{email}</h2>
+        <button onClick={() => {
+          generateEmail()
+        }}>Generate Email</button>
+      </div>
+      
     </div>
   )
 }
